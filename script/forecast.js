817907e970afef8a38a3999a4aa2e5d69a6b75b8
axtcsq <s10194188@connect.np.edu.sg>
2020-12-20 19:04:49 +0800

    Bug fix

--- script/forecast.js
+++ script/forecast.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // @params settings
     var params = {
         // YYYY-MM-DD[T]HH:mm:ss (SGT)
-        "date_time": "2016-06-27T23:00:00", "date": "2016-06-27" // YYYY-MM-DD
+        "date_time": "2020-12-20T17:30:00", "date": "2020-12-20" // YYYY-MM-DD
     };
     
     $.ajax({
@@ -11,18 +11,28 @@ $(document).ready(function() {
         contentType: "text/plain",
         url: "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast",
         headers: {},
-        data: {"date_time": "2016-06-27T23:00:00", "date":"2016-06-27"},
+        data: {"date_time": "2020-12-20T17:30:00", "date":"2020-12-20"},
 
         // crossDomain: true,
         // @data returning JSON data
         success: function(data) {
+            // Regions
             var periods_twenty_four =
-            data.items[3].periods.regions;
+            data.items[0].periods[0].regions;
+            console.log(periods_twenty_four);
             var content = "";
+            
+            // Temperatures
+            var temp = data.items[0].general.temperature;
+            console.log(temp);
             
             $.each(periods_twenty_four, function(key, obj) {
             content += key + ": " + obj + "<br/>";
             });
+
+            $.each(temp, function(key, obj) {
+                content += key + ": " + obj + "<br/>";
+                });
             
             $("#regions").html(content);
             
