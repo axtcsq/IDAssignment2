f5da5fc2e071c253bb38f4be0b95fe8c77d59742
axtcsq <73046004+axtcsq@users.noreply.github.com>
2021-01-07 21:46:03 +0800

    Github Restore

---
+++ scripts/forecast.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    // @params settings
+    var params = {
+        // YYYY-MM-DD[T]HH:mm:ss (SGT)
+        "date_time": "2021-01-06T00:00:00", "date": "2021-01-06" // YYYY-MM-DD
+    };
+    
+    $.ajax({
+        type: "GET",
+        dataType: 'json',
+        contentType: "text/plain",
+        url: "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast",
+        headers: {},
+        data: {"date_time": "2021-01-06T00:00:00", "date":"2021-01-06"},
+
+        // crossDomain: true,
+        // @data returning JSON data
+        success: function(data) {
+            // Regions
+            var periods_twenty_four = data.items[0].periods[0].regions;
+            var content = "";
+            
+            // Temperatures
+            var temp = data.items[0].general.temperature;
+            
+            $.each(periods_twenty_four, function(key, obj) {
+            content += key + ": " + obj + "<br/>";
+            });
+
+            $.each(temp, function(key, obj) {
+                content += key + ": " + obj + "<br/>";
+                });
+            
+            $("#forecast").html(content);
+            
+            // Store info as local storage
+            localStorage.setItem("twenty_four_hourly",JSON.stringify(periods_twenty_four));
+        }
+    })
+})
