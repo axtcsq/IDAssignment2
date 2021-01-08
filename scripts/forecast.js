b940314b8affe79922cdc296eb805dfe7eeb9002
axtcsq <73046004+axtcsq@users.noreply.github.com>
2021-01-08 17:09:39 +0800

    Updated weather data

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // @params settings
     var params = {
         // YYYY-MM-DD[T]HH:mm:ss (SGT)
-        "date_time": "2021-01-07T00:00:00", "date": "2021-01-07" // YYYY-MM-DD
+        "date_time": "2021-01-08T00:00:00", "date": "2021-01-08" // YYYY-MM-DD
     };
     
     $.ajax({
@@ -11,7 +11,7 @@ $(document).ready(function() {
         contentType: "text/plain",
         url: "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast",
         headers: {},
-        data: {"date_time": "2021-01-07T00:00:00", "date":"2021-01-07"},
+        data: {"date_time": "2021-01-08T00:00:00", "date":"2021-01-08"},
 
         // crossDomain: true,
         // @data returning JSON data
